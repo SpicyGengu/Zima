ed9d22631a525ee7931a1f54f1d4d2fef9126676
SpicyGengu <lovro.antic1@gmail.com>
2026-09-21 11:28:31 +1000

    update: 21-09-2026

--- configuration.nix
+++ configuration.nix
@@ -147,11 +147,11 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  nix.gc = {
-    automatic = true;
-    dates = "daily";
-    options = "--delete-older-than +5";
-  };
+  # nix.gc = {
+  #   automatic = true;
+  #   dates = "daily";
+  #   options = "--delete-older-than +5";
+  # };
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
